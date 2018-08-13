56c5c16b0aff49bb1b27eef304399f3b5d14f167
Sungpeo Kook <justkook@gmail.com>
2018-08-13 15:06:59 +0900

    Service Registry - Wiring Eureka Clients

--- components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
+++ components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
@@ -2,6 +2,7 @@ package io.pivotal.pal.tracker.restsupport;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.netflix.discovery.EurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestOperations;
@@ -10,6 +11,17 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestConfig {
+
+    private final EurekaClient eurekaClient;
+
+    public RestConfig(EurekaClient eurekaClient) {
+        this.eurekaClient = eurekaClient;
+    }
+
+    @Bean
+    ServiceLocator getServiceLocator() {
+        return new EurekaServiceLocator(eurekaClient);
+    }
 
     @Bean
     public RestOperations restOperations() {
